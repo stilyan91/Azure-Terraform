02f9a0b35349df3e79da5eb08ca86e1d9ef05304
stilyan91 <stilyan91@gmail.com>
2023-10-18 22:15:12 +0300

    Init repo

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The resource group name"
+}
+
+
+variable "resource_group_location" {
+  type        = string
+  description = "The resource group name"
+}
+variable "app_service_plan_name" {
+  type        = string
+  description = "The resource group name"
+}
+variable "app_service_name" {
+  type        = string
+  description = "The resource group name"
+}
+variable "sql_server_name" {
+  type        = string
+  description = "The resource group name"
+}
+variable "sql_database_name" {
+  type        = string
+  description = "The resource group name"
+}
+variable "sql_administrator_login_username" {
+  type        = string
+  description = "The resource group name"
+}
+variable "sqladministrator_password" {
+  type        = string
+  description = "The resource group name"
+}
+variable "firewall_rule_name" {
+  type        = string
+  description = "The resource group name"
+}
+variable "github_repo_url" {
+  type        = string
+  description = "Repo URL"
+}
+
